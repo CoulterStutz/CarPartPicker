806d5f024437ec96a160d1bcfa59ab7d902792d6
CoulterStutz <coultercash@gmail.com>
2024-03-20 03:25:41 -0600

    [LIST JS]: FIX BUG WHERE YOU CAN SPAM EMPTY CELLS

--- list/script.js
+++ list/script.js
@@ -1,19 +1,21 @@
 function addRow(selectedComponent) {
     if (!document.getElementById(selectedComponent)) {
-        var table = document.querySelector(".carbuilder tbody");
-        var row = table.insertRow(-1);
-        row.id = selectedComponent;
+            var table = document.querySelector(".carbuilder tbody");
+            var row = table.insertRow(-1);
+            row.id = selectedComponent;
 
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
-        var cell5 = row.insertCell(4);
+            if (selectedComponent != ""){
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                var cell3 = row.insertCell(2);
+                var cell4 = row.insertCell(3);
+                var cell5 = row.insertCell(4);
 
-        cell1.innerHTML = selectedComponent;
-        cell2.innerHTML = "";
-        cell3.innerHTML = "";
-        cell4.innerHTML = "";
-        cell5.innerHTML = "";
+                cell1.innerHTML = selectedComponent;
+                cell2.innerHTML = "";
+                cell3.innerHTML = "";
+                cell4.innerHTML = "";
+                cell5.innerHTML = "";
+            }
     }
 }
