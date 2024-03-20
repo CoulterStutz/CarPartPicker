070566650c7cf8a9d0bd5cfc77148c1244a07a68
CoulterStutz <coultercash@gmail.com>
2024-03-20 03:47:15 -0600

    [LIST JS]: RESET DROPDOWN AFTER SELECTION

--- list/script.js
+++ list/script.js
@@ -1,21 +1,22 @@
 function addRow(selectedComponent) {
-    if (!document.getElementById(selectedComponent)) {
-            var table = document.querySelector(".carbuilder tbody");
-            var row = table.insertRow(-1);
-            row.id = selectedComponent;
+    if (!document.getElementById(selectedComponent) && selectedComponent != "") {
+        var table = document.querySelector(".carbuilder tbody");
+        var row = table.insertRow(-1);
+        row.id = selectedComponent;
 
-            if (selectedComponent != ""){
-                var cell1 = row.insertCell(0);
-                var cell2 = row.insertCell(1);
-                var cell3 = row.insertCell(2);
-                var cell4 = row.insertCell(3);
-                var cell5 = row.insertCell(4);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
 
-                cell1.innerHTML = selectedComponent;
-                cell2.innerHTML = "<button>+</button>";
-                cell3.innerHTML = "";
-                cell4.innerHTML = "";
-                cell5.innerHTML = "";
-            }
+        cell1.innerHTML = selectedComponent;
+        cell2.innerHTML = "<button>+</button>";
+        cell3.innerHTML = "";
+        cell4.innerHTML = "";
+        cell5.innerHTML = "";
+
+        // Reset the dropdown menu
+        document.querySelector("select[name='components']").value = "";
     }
 }
